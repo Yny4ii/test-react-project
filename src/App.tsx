3b50e36b2a750b2630c912c701300b8732cbd8b0
Ilya Semyonov <ilya.semyonov.03@mail.ru>
2023-03-26 14:05:23 +0300

    feat: add task logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+interface ParamInputProps {
+    label: string;
+    value: string;
+    onChange: (value: string) => void;
+}
+
+const ParamInput: React.FC<ParamInputProps> = ({label, value, onChange}) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <input type="text" value={value} onChange={(e) => onChange(e.target.value)}/>
+        </div>
+    );
+};
+
+interface Param {
+    id: string;
+    label: string;
+    value: string;
+}
+
+interface Model {
+    id: string;
+    name: string;
+    params: Param[];
+}
+
+interface ModelEditorProps {
+    model: Model;
+}
+
+const ModelEditor: React.FC<ModelEditorProps> = ({model}) => {
+    const [params, setParams] = useState<Param[]>(model.params);
+
+    const handleParamChange = (paramId: string, value: string) => {
+        const updatedParams = params.map((param) => {
+            if (param.id === paramId) {
+                return {...param, value};
+            }
+            return param;
+        });
+        setParams(updatedParams);
+    };
+
+    const getModel = (): Model => {
+        return {
+            ...model,
+            params,
+        };
+    };
+
+    return (
+        <div>
+            <h2>{model.name}</h2>
+            {params.map((param) => (
+                <ParamInput
+                    key={param.id}
+                    label={param.label}
+                    value={param.value}
+                    onChange={(value) => handleParamChange(param.id, value)}
+                />
+            ))}
+            <button onClick={() => console.log(getModel())}>Save</button>
+        </div>
+    );
+};
+
+
+const models = [
+    {
+        id: "1",
+        name: "Model 1",
+        params: [
+            {id: "1", label: "Param 1", value: "Value 1"},
+            {id: "2", label: "Param 2", value: "Value 2"},
+        ],
+    },
+    {
+        id: "2",
+        name: "Model 2",
+        params: [
+            {id: "3", label: "Param 3", value: "Value 3"},
+            {id: "4", label: "Param 4", value: "Value 4"},
+        ],
+    },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {models.map((model) => (
+                <ModelEditor key={model.id} model={model}/>
+            ))}
+        </div>
+    );
 }
 
 export default App;
